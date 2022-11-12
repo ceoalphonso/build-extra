8b57379cf3e9af749ce4faf1d4d3526696a84e1a
ceoalphonso <75475883+ceoalphonso@users.noreply.github.com>
2022-11-12 06:38:25 +0000

    Initial commit

---
+++ download-stats.sh
@@ -0,0 +1,206 @@
+#!/bin/sh
+
+# This script shows the download stats on GitHub. Update the ids by calling
+# this script with the `--update` option
+
+test "--update" != "$1" || {
+	curl -s \
+	 https://api.github.com/repos/git-for-windows/git/releases |
+	tac |
+	sed -n '/^  }/{
+    :1;N;/\n  {/b2;b1
+    :2;
+    y/\n/\r/
+    s/.*"tag_name": "\([^"]*\)".*"id": \([0-9]*\).*/# \1\n#id=${1:-\2}/p
+}' | sed '$s/^#//' >"${0%.sh}".ids &&
+	echo "$ids" |
+	sed -i -e '/^#\( v\?2.*windows\|id=\)/d' -e '/^id=/d' \
+		-e "/^# IDs/r${0%.sh}.ids" "$0"
+	exit
+}
+
+# IDs
+# v2.35.0-rc0.windows.1
+#id=${1:-56836871}
+# v2.35.0-rc1.windows.1
+#id=${1:-57179696}
+# v2.35.0-rc2.windows.1
+#id=${1:-57513049}
+# v2.35.0.windows.1
+#id=${1:-57875366}
+# v2.35.1.windows.1
+#id=${1:-58246350}
+# v2.35.1.windows.2
+#id=${1:-58447316}
+# v2.32.1.windows.1
+#id=${1:-64275626}
+# v2.35.2.windows.1
+#id=${1:-64269813}
+# v2.36.0-rc0.windows.1
+#id=${1:-63655404}
+# v2.36.0-rc1.windows.1
+#id=${1:-64012346}
+# v2.35.3.windows.1
+#id=${1:-64509402}
+# v2.36.0-rc2.windows.1
+#id=${1:-64661937}
+# v2.36.0.windows.1
+#id=${1:-64857903}
+# v2.36.1.windows.1
+#id=${1:-66402762}
+# v2.37.0-rc0.windows.1
+#id=${1:-69510180}
+# v2.37.0-rc1.windows.1
+#id=${1:-69764973}
+# v2.37.0-rc2.windows.1
+#id=${1:-70092130}
+# v2.37.0.windows.1
+#id=${1:-70675600}
+# v2.35.4.windows.1
+#id=${1:-71825092}
+# v2.36.2.windows.1
+#id=${1:-71825145}
+# v2.37.1.windows.1
+#id=${1:-71824978}
+# v2.37.2.windows.1
+#id=${1:-74247192}
+# v2.37.2.windows.2
+#id=${1:-74254206}
+# v2.37.3.windows.1
+#id=${1:-75831597}
+# v2.38.0-rc0.windows.1
+#id=${1:-77353845}
+# v2.38.0-rc1.windows.1
+#id=${1:-77863153}
+# v2.38.0-rc2.windows.1
+#id=${1:-78375228}
+# v2.38.0.windows.1
+#id=${1:-78876064}
+# v2.38.1.windows.1
+#id=${1:-80235068}
+# v2.35.5.windows.1
+id=${1:-80234950}
+# v2.11.1.mingit-prerelease.6
+# v2.11.1.mingit-prerelease.4
+# v2.11.1.mingit-prerelease.5
+# v2.11.1.mingit-prerelease.6
+# v2.11.1.mingit-prerelease.4
+# v2.11.1.mingit-prerelease.5
+# v2.11.1.mingit-prerelease.6
+# v2.11.1.mingit-prerelease.4
+# v2.11.1.mingit-prerelease.5
+# v2.11.1.mingit-prerelease.6
+# v2.11.1.mingit-prerelease.4
+# v2.11.1.mingit-prerelease.5
+# v2.11.1.mingit-prerelease.6
+# v2.11.1.mingit-prerelease.4
+# v2.11.1.mingit-prerelease.5
+# v2.11.1.mingit-prerelease.6
+# v2.11.1.mingit-prerelease.4
+# v2.11.1.mingit-prerelease.5
+# v2.11.1.mingit-prerelease.4
+# v2.11.1.mingit-prerelease.5
+# v2.11.1.mingit-prerelease.3
+# v2.11.1.mingit-prerelease.3
+# v2.11.1.mingit-prerelease.3
+# v2.11.1.mingit-prerelease.3
+# v2.11.1.mingit-prerelease.3
+# v2.11.1.mingit-prerelease.3
+# v2.11.1.mingit-prerelease.3
+# v2.11.1.mingit-prerelease.3
+# v2.11.1.mingit-prerelease.2
+# v2.11.1.mingit-prerelease.2
+# v2.11.1.mingit-prerelease.2
+# v2.11.1.mingit-prerelease.2
+# v2.11.1.mingit-prerelease.2
+# v2.11.1.mingit-prerelease.2
+# v2.11.1.mingit-prerelease.2
+# v2.11.1.mingit-prerelease.2
+# v2.11.1.mingit-prerelease.2
+# v2.11.1.mingit-prerelease.2
+# v2.11.1.mingit-prerelease.1
+# v2.11.1.mingit-prerelease.2
+# v2.11.1.mingit-prerelease.1
+# v2.11.1.mingit-prerelease.2
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# v2.11.1.mingit-prerelease.2
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# v2.11.1.mingit-prerelease.2
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# v2.11.1.mingit-prerelease.2
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# v2.11.1.mingit-prerelease.2
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# v2.11.1.mingit-prerelease.2
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# prerelease-v2.10.0.windows.1.11.geda474c
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# prerelease-v2.10.0.windows.1.11.geda474c
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# prerelease-v2.10.0.windows.1.11.geda474c
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# prerelease-v2.10.0.windows.1.11.geda474c
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# prerelease-v2.10.0.windows.1.11.geda474c
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# prerelease-v2.10.0.windows.1.11.geda474c
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# prerelease-v2.10.0.windows.1.11.geda474c
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# prerelease-v2.10.0.windows.1.11.geda474c
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# prerelease-v2.10.0.windows.1.11.geda474c
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# prerelease-v2.10.0.windows.1.11.geda474c
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# prerelease-v2.10.0.windows.1.11.geda474c
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# prerelease-v2.10.0.windows.1.11.geda474c
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# prerelease-v2.10.0.windows.1.11.geda474c
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# prerelease-v2.10.0.windows.1.11.geda474c
+# prerelease-v2.11.0.windows.1.1
+# v2.11.1.mingit-prerelease.1
+# prerelease-v2.10.0.windows.1.11.geda474c
+# prerelease-v2.10.0.windows.1.11.geda474c
+# prerelease-v2.10.0.windows.1.11.geda474c
+
+case "$id" in
+*.*)
+	case "$id" in
+	v*.windows.*) id="$(echo "$id" | sed 's/\./\\./g')";;
+	*\(*\)) id="$(echo "$id" |
+		sed -e 's/(\(.*\))$/.windows.\1/' -e 's/\./\\./g')";;
+	*)
+		id="$(echo "$id" | sed 's/\./\\./g').windows.1";;
+	esac
+	id="$(sed -n "/$id/{N;s/.*:-\([0-9]*\).*/\1/p}" <"$0")"
+	test -n "$id" || {
+		echo "Version $1 not found" >&2
+		exit 1
+	}
+	;;
+esac
+
+curl -s https://api.github.com/repos/git-for-windows/git/releases/$id/assets |
+grep -e '"name":' -e '"download_count":'
